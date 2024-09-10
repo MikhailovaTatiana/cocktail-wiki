e834e43b113550a24ebaa841b3426804dd49a872
Fanny <fsb62@hotmail.com>
2024-09-10 15:14:28 +0200

    Navbar komponent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
 import "../styles/App.css";
 import { Outlet } from "react-router-dom";
+import Navbar from "./NavBar";
 
 function App() {
     return (
         <>
+        <Navbar/>
             <h1>App</h1>
+            
             <Outlet />
+
         </>
     );
 }
