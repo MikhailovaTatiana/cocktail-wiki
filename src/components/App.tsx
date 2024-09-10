11a6a6899c4987644e017cd17632390840a3b2db
Tatiana Mikhailova <imagetata@gmail.com>
2024-09-10 09:24:16 +0100

    project's structure & router setting

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+import { Outlet } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <h1>App</h1>
+            <Outlet />
+        </>
+    );
+}
+
+export default App;
