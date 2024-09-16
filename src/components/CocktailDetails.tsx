ab9a524db61da46558a87fc7aebffed086edec55
louohr <louise.ohrner@gmail.com>
2024-09-16 13:43:02 +0200

    fix html tags and add css

--- src/components/CocktailDetails.tsx
+++ src/components/CocktailDetails.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "../styles/DrinkInfo.css";
 
 interface Cocktail {
   idDrink: string; // id
   strDrink: string; // namn
   strDrinkThumb: string; // bild
-  strInstructions: string; // instruktioner
   strCategory: string; // kategori
   strAlcoholic: string; // alkohol
   strTags: string; // taggar
@@ -38,38 +38,43 @@ const CocktailDetails: React.FC = () => {
   if (!cocktail) return <p>Erorr loading api</p>;
 
   return (
-    <section>
-      <h1>{cocktail.strDrink}</h1>
-      <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
-      <p>Category: {cocktail.strCategory}</p>
-      <p>Type: {cocktail.strAlcoholic}</p>
-      <p>Glass: {cocktail.strGlass}</p>
-      <h1>List of ingredients:</h1>
-      <ul>
-        <li>
-          {cocktail.strMeasure1}
-          {cocktail.strIngredient1}
-        </li>
-        <li>
-          {cocktail.strMeasure2}
-          {cocktail.strIngredient2}
-        </li>
-        <li>
-          {cocktail.strMeasure3}
-          {cocktail.strIngredient3}
-        </li>
-        <li>
-          {cocktail.strMeasure4}
-          {cocktail.strIngredient4}
-        </li>
-        <li>
-          {cocktail.strMeasure5}
-          {cocktail.strIngredient5}
-        </li>
-      </ul>
-      <h1>Instructions:</h1>
-      <p>{cocktail.strInstructions}</p>
-    </section>
+    <main className="info-main">
+      <h1 className="info-header">Drink Information</h1>
+      <section className="info-card">
+        <aside className="info-aside info-right">
+          <h1 className="drink-header">{cocktail.strDrink}</h1>
+          <img className="info-img" src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+          <p>Category: {cocktail.strCategory}</p>
+          <p>Type: {cocktail.strAlcoholic}</p>
+          <p>Glass: {cocktail.strGlass}</p>
+        </aside>
+        <aside className="info-aside info-left">
+          <h1>Ingredients:</h1>
+          <ul>
+            <li>
+              {cocktail.strMeasure1}
+              {cocktail.strIngredient1}
+            </li>
+            <li>
+              {cocktail.strMeasure2}
+              {cocktail.strIngredient2}
+            </li>
+            <li>
+              {cocktail.strMeasure3}
+              {cocktail.strIngredient3}
+            </li>
+            <li>
+              {cocktail.strMeasure4}
+              {cocktail.strIngredient4}
+            </li>
+            <li>
+              {cocktail.strMeasure5}
+              {cocktail.strIngredient5}
+            </li>
+          </ul>
+        </aside>
+      </section>
+    </main>
   );
 };
 
