7a9f5622856b237780b9889d6e171b97f33d78fd
Fanny <fsb62@hotmail.com>
2024-09-17 13:23:25 +0200

    justerat css

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -66,15 +66,32 @@ export function Search() {
     }
   };
 
-  return (
+  const shouldShowPagination = searchPerformed && cocktails.length > 0 && totalPages > 1;
+
+  return (    
     <main className="search-main">
       <h1 className="search-header">Search Results for "{query}"</h1>
-      <div className="pagination-arrows">
+      {shouldShowPagination && (
+  <div className="pagination-container">
+    <div className="arrow-placeholder">
       {currentPage > 1 && (
-          <button className='arrow-btn' onClick={handlePrevPage} /* disabled={currentPage === 1} */>
-            <img src={LeftArrowIcon} alt="Previous Page" />
-          </button>
-        )}
+        <button className='arrow-btn' onClick={handlePrevPage}>
+          <img src={LeftArrowIcon} alt="Previous Page" />
+        </button>
+      )}
+    </div>
+    <span className="pages">
+      <p> Page {currentPage} of {totalPages}</p>
+    </span>
+    <div className="arrow-placeholder">
+      {currentPage < totalPages && (
+        <button className='arrow-btn' onClick={handleNextPage}>
+          <img src={RightArrowIcon} alt="Next Page" />
+        </button>
+      )}
+    </div>
+  </div>
+)}      
       {loading ? (
         <p>Loading...</p>
       ) : (
@@ -98,25 +115,7 @@ export function Search() {
             <p>Search for your favorite cocktails.</p>
           )}
         </section>
-        
-      )}{currentPage < totalPages && (
-          <button className='arrow-btn' onClick={handleNextPage} disabled={currentPage === totalPages}>
-            <img src={RightArrowIcon}  alt="Next Page" />
-          </button>
-        )}
-      </div>
-
-
-
-      {/* Pagination Controls */}
-
-      <div className="pagination">
-       
-        <span>
-          Page {currentPage} of {totalPages}
-        </span>
-       
-      </div>
+      )}
     </main>
   );
 }
