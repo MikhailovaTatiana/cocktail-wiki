4bdd33647b8b957e8d8e3507cf83c0b714a7bddd
Tatiana Mikhailova <imagetata@gmail.com>
2024-09-19 15:13:04 +0200

    empty favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import '../styles/Favorites.css';
-import { useFavorites } from '../context/FavoritesContext';
+import React from "react";
+import "../styles/Favorites.css";
+import { useFavorites } from "../context/FavoritesContext";
 
 const Favorites = () => {
-  const { favorites, removeFavorite } = useFavorites(); // Hämta favoriter och removeFavorite från Context
+    const { favorites, removeFavorite } = useFavorites(); // Hämta favoriter och removeFavorite från Context
 
-  return (
-    <div className="container">
-      <h1>Favorites</h1>
-
-      {/* Visa meddelande om inga favoriter finns */}
-      {favorites.length === 0 ? (
-        <p>Du har inga sparade favoriter</p>
-      ) : (
-        <div className="grid">
-          {favorites.map((drink, index) => (
-            <div key={index} className="card">
-              <img src={drink.imgUrl} alt={drink.name} />
-              <h2>{drink.name}</h2>
-              <button onClick={() => removeFavorite(drink.name)}>Remove</button>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    return (
+        <main className="favorites-main">
+            <h1 className="fav-header">Favorites</h1>
+            {/* Visa meddelande om inga favoriter finns */}
+            {favorites.length === 0 ? (
+                <p className="empty">Du har inga sparade favoriter</p>
+            ) : (
+                <div className="grid">
+                    {favorites.map((drink, index) => (
+                        <div key={index} className="card">
+                            <img src={drink.imgUrl} alt={drink.name} />
+                            <h2>{drink.name}</h2>
+                            <button onClick={() => removeFavorite(drink.name)}>
+                                Remove
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </main>
+    );
 };
 
 export default Favorites;
-
-
